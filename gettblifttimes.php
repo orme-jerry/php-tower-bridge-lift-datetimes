b306b7f0dda6d67b70d8573de6572addf9ca6d0e
orme-jerry <orme.jerry@googlemail.com>
2016-09-03 17:30:28 +0100

    Include multi_query() and truncate table

--- gettblifttimes.php
+++ gettblifttimes.php
@@ -1,24 +1,24 @@
 <?php
 
 	function print_r2($val) {
-        echo '<pre>';
-        print_r($val);
-        echo  '</pre>';
+		echo '<pre>';
+		print_r($val);
+		echo  '</pre>';
 	}
 
 	function map2darray($Headers, $Elements) {
-		//Get header names of the table
+		//get header names of the table
 		foreach($Headers as $Header) {
-		$headers_array[] = trim($Header->textContent);
-	  }
+			$headers_array[] = trim($Header->textContent);
+		}
 		
-		//Get table elements and assign integer to row key, and Header to column key
+		//get table elements and assign integer to row key, and Header to column key
 		$elements = 0;
 		$rows = 0;
 		$noOfHeaders = count($headers_array);
 		foreach($Elements as $Element){
 			$elements_2darray[$rows][$headers_array[$elements % $noOfHeaders]] = trim($Element->textContent);
-			#begin new "row" when $elements reaches 5, 10, 15...
+			//begin new "row" when $elements reaches 5, 10, 15...
 			$elements = $elements + 1;
 			if ($elements % $noOfHeaders == 0) {
 				$rows = $rows + 1;
@@ -42,14 +42,12 @@
 	$earliestMonth = DateTime::createFromFormat('d M', $table[0]["Date"])->format("m");
 
 	//date-time handling and conversion to ISO8601
-	//coercion of Date and Time fields into a single Datetime field
+	//combination of Date and Time fields into a single Datetime field
 	$rows = count($table);
-	for($row = 0; $row < $rows; $row++)
-	{
+	for($row = 0; $row < $rows; $row++) {
 		$dateTime = DateTime::createFromFormat("d MH:i", "{$table[$row]["Date"]}{$table[$row]["Time"]}");
 		
-		if($dateTime->format("m") < $earliestMonth)
-		{
+		if($dateTime->format("m") < $earliestMonth) {
 			$dateTime = $dateTime->modify("+1 year");#handles dates that fall in the following year
 		}
 		$db[$row]["datetime"] = $dateTime->format("Y-m-d-H-i");
@@ -57,21 +55,20 @@
 		$db[$row]["direction"] = $table[$row]["Direction"];
 	}
 
-	// Implosion of $db for SQL quert
+	//implosion of $db for SQL script
 	$columns = implode("`, `", array_keys($db[1]));
 	$escaped_values = implode("'), ('", array_map(function ($entry) {
 	  return implode("','", $entry);
 	}, $db));
-	$sql = "INSERT INTO `lifttimes`(`$columns`) VALUES ('$escaped_values')  ON DUPLICATE KEY UPDATE datetime=datetime, vessel=vessel, direction=direction";
-
-  // Server credentials
+	$sql = "TRUNCATE TABLE `lifttimes`; ALTER TABLE `lifttimes` AUTO_INCREMENT = 1; INSERT INTO `lifttimes` (`$columns`) VALUES ('$escaped_values') ON DUPLICATE KEY UPDATE datetime=datetime, vessel=vessel, direction=direction";
+	//echo $sql;
 	$servername = "***";
 	$username = "***";
 	$password = "***";
 	$dbname = "***";
 	$port = ***;
 	
-	// Open log
+	//open log
 	$myFile = "***";
 	$fh2 = fopen($myFile, 'a');
 	if ($fh2 === FALSE) {
@@ -82,13 +79,13 @@
 		$stringData = "$ctime\t";
 		fwrite($fh2, $stringData);
 
-		// Create and check connection
+		//create and check connection
 		$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 		if ($conn->connect_error) {
 			fwrite($fh2, $conn->connect_error);			
 		}
-		else if ($conn->query($sql) === TRUE) {
+		else if ($conn->multi_query($sql) === TRUE) {
 			fwrite($fh2, "update successful");
 			$conn->close();
 		}
